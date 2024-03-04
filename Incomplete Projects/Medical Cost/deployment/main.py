56435324a2af00072d89a2a14fc25fa35941b835
Ankit kumar <ankitkumar9864@gmail.com>
2024-03-04 12:05:34 +0530

    03_03_2024

--- Incomplete Projects/Medical Cost/deployment/main.py
+++ Incomplete Projects/Medical Cost/deployment/main.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
 import joblib
 import pandas as pd
-import pickle
 import numpy as np
+
 app = Flask(__name__)
 
 # Load the pre-trained linear regression model
-model = joblib.load('deployment\models\insurance_linear_regression.plk')
+model = joblib.load('models/insurance_linear_regression.plk')
 
 @app.route('/')
 def home():
@@ -19,16 +19,41 @@ def predict():
         age = float(request.form['age'])
         bmi = float(request.form['bmi'])
         children = int(request.form['children'])
-        sex = object(request.form['sex'])
-        smoker = object(request.form['smoker'])
-        region = object(request.form['region'])
+        sex = request.form['sex']
+        smoker = request.form['smoker']
+        region = request.form['region']
         
+        # Convert categorical variables into numerical values
+        sex = 1 if sex.lower() == 'male' else 0
+        smoker = 1 if smoker.lower() == 'yes' else 0
+        
+        # Transform region into dummy variables
+        if region.lower() == 'southwest':
+            region_southwest = 1
+            region_southeast = 0
+            region_northeast = 0
+            region_northwest = 0
+        elif region.lower() == 'southeast':
+            region_southwest = 0
+            region_southeast = 1
+            region_northeast = 0
+            region_northwest = 0
+        elif region.lower() == 'northeast':
+            region_southwest = 0
+            region_southeast = 0
+            region_northeast = 1
+            region_northwest = 0
+        else:  # assume 'northwest' as default
+            region_southwest = 0
+            region_southeast = 0
+            region_northeast = 0
+            region_northwest = 1
         
         # Predict insurance charges
-        prediction = model.predict([['age', 'sex', 'bmi', 'children', 'smoker', 'region']])
+        prediction = model.predict([[age, sex, bmi, children, smoker, region_southwest, region_southeast, region_northeast, region_northwest]])
         
         # Render the result template with the prediction
-        return render_template('home.html', prediction=prediction)
+        return render_template('result.html', prediction=prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
